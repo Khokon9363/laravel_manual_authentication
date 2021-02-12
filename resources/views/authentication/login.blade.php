1a1c74acbb0e100a87673739cbd2af0bb688edb4
khokon9363 <khokon9363@gmail.com>
2021-02-12 17:56:35 +0600

    Manual Authentication

---
+++ resources/views/authentication/login.blade.php
@@ -0,0 +1,17 @@
+@extends('authentication.master')
+@section('contents')
+    <div class="card-header">Login</div>
+    <div class="card-body">
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <span class="text-danger">
+                    {{ session('error') }}
+                </span>
+                <input type="text" name="email" class="form-control mb-3" placeholder="Enter your email">
+                <input type="password" name="password" class="form-control mb-3" placeholder="Enter your password">
+                <button type="submit" class="btn btn-sm btn-info">Login</button>
+            </div>
+        </form>
+    </div>
+@endsection
